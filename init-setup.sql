085c16f20f801fe1c4194077e8f8bc9219e7f33f
Lucas Castro <lfsc09@gmail.com>
2024-04-13 01:36:44 -0300

    feat: runnable container with initial mysql setup

---
+++ init-setup.sql
@@ -0,0 +1,29 @@
+CREATE USER 'user'@'localhost' IDENTIFIED BY 'sFg$8d7QW@sd';
+
+GRANT
+    ALTER,
+    ALTER ROUTINE,
+    CREATE,
+    CREATE ROUTINE,
+    CREATE TABLESPACE,
+    CREATE TEMPORARY TABLES,
+    CREATE VIEW,
+    DELETE,
+    DROP,
+    EVENT,
+    EXECUTE,
+    FILE,
+    INDEX,
+    INSERT,
+    LOCK TABLES,
+    PROCESS,
+    REFERENCES,
+    RELOAD,
+    SELECT,
+    SHOW DATABASES,
+    SHOW VIEW,
+    TRIGGER,
+    UPDATE
+ON *.* TO 'user'@'localhost';
+
+FLUSH PRIVILEGES;
